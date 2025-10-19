71c83ff50ba4a1b45790802551a31a7e61327971
Lain <ivan.shankin04@gmail.com>
2025-10-19 06:18:42 +0300

    CI/CD. Change of java version.

--- service-api/src/main/java/greencity/service/GoogleAuthService.java
+++ service-api/src/main/java/greencity/service/GoogleAuthService.java
@@ -2,10 +2,8 @@ package greencity.service;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-
 import java.net.URI;
 
 public interface GoogleAuthService {
-
     URI generateGoogleAuthRedirectUrl(HttpServletRequest request, HttpServletResponse response);
 }
